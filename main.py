ff50e8ee9d6b00a7e765b2d6d28fff42fb01f263
Nafiz0081 <sameurrahman99@gmail.com>
2026-07-28 21:28:43 +0600

    Added telegram token

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder
 def main():
     print("Hello from hr-bot!")
     
-    app = ApplicationBuilder().token("YOUR_BOT_TOKEN_HERE").build()
+    app = ApplicationBuilder().token("8937341187:AAHjEe_yOgi0D-EJw15p9oBE7A8jiJFKB-0").build()
     
     # This line blocks forever — exactly what Render needs
     app.run_polling()
